c35f0cb33066a197118ba08c421a525ffcb69574
Daksh-134 <117268999+Daksh-134@users.noreply.github.com>
2024-07-15 00:24:35 +0530

    MenuMate

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -20,7 +20,7 @@ export default function nav() {
   return (
     <nav className="navbar navbar-expand-lg bg-success">
       <div className="container-fluid">
-        <Link className="navbar-brand fs-2 fst-italic" to="#">ZWIGATO</Link>
+        <Link className="navbar-brand fs-2 fst-italic" to="#">MENU-MATE</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
