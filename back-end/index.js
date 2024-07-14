000a9305a2b1e5c664746a3702c7b5be97ea494d
Daksh-134 <117268999+Daksh-134@users.noreply.github.com>
2024-07-15 01:04:40 +0530

    Link final change

--- back-end/index.js
+++ back-end/index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 })
 // Cross Origin Resource Sharing
 app.use(cors({
-  origin: 'https://menumate123.vercel.app/'
+  origin: 'https://menumate123.vercel.app'
 }));
 
 app.use(express.json())
